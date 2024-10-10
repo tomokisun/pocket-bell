b8a7056ad8d6ef04e885007bf03b28b43bed4159
tomokisun <github@tomokisun.com>
2024-10-10 19:17:38 +0900

    feat: minimum ios 17

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
   name: "pocket-bell",
   platforms: [
-    .iOS(.v18),
+    .iOS(.v17),
   ],
   products: [
     .library(name: "APIClient", targets: ["APIClient"]),
