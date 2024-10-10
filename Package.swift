04f71f2f6ff547676c059e444ca221062242c105
tomokisun <github@tomokisun.com>
2024-10-10 17:34:23 +0900

    feat: minimum version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "pocket-bell",
   platforms: [
-    .iOS(.v17),
+    .iOS(.v18),
   ],
   products: [
     .library(name: "APIClient", targets: ["APIClient"]),
